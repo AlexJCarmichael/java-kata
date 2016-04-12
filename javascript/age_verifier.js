d6989a940228c293cedfe4d99a5e1a1102e81d01
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-12 13:11:10 -0500

    Finished Java age verifier.

---
+++ javascript/age_verifier.js
@@ -0,0 +1,13 @@
+
+var ageVerifier = function(age) {
+  if (age < 21) {
+    console.log("Too young.");
+  }
+  else {
+      console.log("Proceed");
+  }
+};
+
+ageVerifier(18)
+ageVerifier(21)
+ageVerifier(50)
