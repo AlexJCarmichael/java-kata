5d2c9d9835e18d5c47c96849d859a0c6320971c3
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-12 13:28:30 -0500

    Finished Fizzbuzz for Ruby.

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,17 +1,26 @@
 
 def fizzbuzz(n)
-  if n%5 == 0
+  if n%15 == 0
+    puts "FizzBuzz"
+  elsif n%5 == 0
     puts "Buzz"
   elsif n % 3 == 0
     puts "Fizz"
   else
-    puts "idlsufabsldkfhasdkjhfk"
+    puts n
   end
 end
 
+100.times do |x|
+  fizzbuzz(x+1)
+end
 
 fizzbuzz(3)
 fizzbuzz(6)
 fizzbuzz(9)
 fizzbuzz(10)
 fizzbuzz(15)
+fizzbuzz(30)
+fizzbuzz(25)
+fizzbuzz(35)
+fizzbuzz(100)
