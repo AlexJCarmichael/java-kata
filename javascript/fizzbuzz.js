1928a82ea2c52062e8dc3d555dbdba132ba1244a
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-12 13:35:01 -0500

    Fizz and Buzz working for JS fizzbuzz.

---
+++ javascript/fizzbuzz.js
@@ -0,0 +1,22 @@
+
+var fizzBuzz = function(n) {
+  if (n%5 === 0) {
+    console.log("Fizz");
+  }
+  else if (n%3 === 0) {
+    console.log("Buzz");
+  }
+  else{
+    console.log(n)
+  }
+};
+
+fizzBuzz(3)
+fizzBuzz(6)
+fizzBuzz(9)
+fizzBuzz(20)
+fizzBuzz(10)
+fizzBuzz(20)
+fizzBuzz(50)
+fizzBuzz(35)
+fizzBuzz(23)
