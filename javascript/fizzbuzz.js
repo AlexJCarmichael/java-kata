bea29b1ad660e563e5b8735fcc4219dddacd75c0
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-12 13:42:03 -0500

    Finished fizzbuzz in JS

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -1,6 +1,9 @@
 
 var fizzBuzz = function(n) {
-  if (n%5 === 0) {
+  if (n%15 === 0) {
+    console.log("FizzBuzz");
+  }
+  else if (n%5 === 0) {
     console.log("Fizz");
   }
   else if (n%3 === 0) {
@@ -15,8 +18,12 @@ fizzBuzz(3)
 fizzBuzz(6)
 fizzBuzz(9)
 fizzBuzz(20)
-fizzBuzz(10)
+fizzBuzz(15)
 fizzBuzz(20)
 fizzBuzz(50)
-fizzBuzz(35)
+fizzBuzz(30)
 fizzBuzz(23)
+
+for (var number = 0; number <= 100; number ++) {
+  fizzBuzz(number)
+};
